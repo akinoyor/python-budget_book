1385dee4e090f5df1a75524b862b075f6971dc31
akinoyor <155416793+akinoyor@users.noreply.github.com>
2024-07-02 19:04:02 +0900

    [add]input window

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # 「簡易家計簿アプリ」を作成
 ## 機能　収入と支出の記録を管理し、月ごとの収支を計算することができます
 ## 目的　tkinter等GUIの仕様練習及び　プログラミング練習
+### 簡易設計　https://drive.google.com/file/d/1am4-sksKLt81lU7ZhtN-BLuaMstRj8EX/view?usp=sharing
--- budget_book.py
+++ budget_book.py
@@ -1,23 +1,58 @@
 import tkinter as tk
 import sys
-WINDOW_SIZE="400x300"
+WINDOW_SIZE="500x200"
+
+class Record:
+    def _init_(self, date, amount, content):
+        self.date = date
+        self.amount = amount
+        self.content = content
 
 def input_window():
     input_window = tk.Tk()
     input_window.title('入力')
     input_window.geometry(WINDOW_SIZE)
+    entry_frame = tk.Frame(input_window)
+    input_buttons_frame = tk.Frame(input_window)
+    entry_frame.grid(row=0, sticky=tk.EW)
+    input_buttons_frame.grid(row=1, column=0, pady=10,sticky=tk.EW)
+    input_window.grid_columnconfigure(0, weight=1)
 
-    exit_button = tk.Button(input_window, text='閉じる', command=input_window.destroy)
-    exit_button.pack(fill='x', side='bottom')
+    date_label = tk.Label(entry_frame, text='日付')
+    date_entry = tk.Entry(entry_frame)
+    amount_label = tk.Label(entry_frame, text='金額')
+    amount_entry = tk.Entry(entry_frame)
+    content_label = tk.Label(entry_frame, text='内容')
+    content_entry = tk.Entry(entry_frame)
+    entry_button = tk.Button(input_buttons_frame, text='追加', command=input(date_entry, amount_entry, content_entry))
+    close_button = tk.Button(input_buttons_frame, text='閉じる', command=input_window.destroy)
+
+    date_label.grid(row=0, column=0, padx=5)
+    date_entry.grid(row=1, column=0, padx=5)
+    content_label.grid(row=0, column=1, sticky=tk.EW)
+    content_entry.grid(row=1, column=1, sticky=tk.EW)
+    amount_label.grid(row=0, column=2, padx=5, sticky=tk.EW)
+    amount_entry.grid(row=1, column=2, padx=5, sticky=tk.EW)
+    entry_frame.grid_columnconfigure(1, weight=3, minsize='200')
+    entry_frame.grid_columnconfigure(2, weight=1, minsize='100')
+    entry_button.grid(row=0, column=0, padx=130)
+    close_button.grid(row=0, column=1)
+
     input_window.mainloop()
+
+def input(date_entry, amount_entry, content_entry):
+    date = date_entry.get()
+    amount = amount_entry.get()
+    content = content_entry.get()
+    print(date,amount,content)
 
 def totalling_window():
     totalling_window = tk.Tk()
     totalling_window.title('集計')
     totalling_window.geometry(WINDOW_SIZE)
 
-    exit_button = tk.Button(totalling_window, text='閉じる', command=totalling_window.destroy)
-    exit_button.pack(fill='x', side='bottom')
+    close_button = tk.Button(totalling_window, text='閉じる', command=totalling_window.destroy)
+    close_button.pack(fill='x', side='bottom')
     totalling_window.mainloop()
 
 root = tk.Tk()
@@ -32,7 +67,4 @@ exit_button.pack(fill='x', side='bottom')
 input_button.pack(fill='x')
 totalling_button.pack(fill='x')
 
-
 root.mainloop()
-
-
